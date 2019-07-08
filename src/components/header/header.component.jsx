c6c75916af0aca5e71b51b8a77583a7758b23c27
IAmConorNolan <nolanc14@tcd.ie>
2019-07-08 02:12:53 +0100

    added nav

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './header.styles.scss';
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/img/crown.svg';
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='option-list'>
+            <Link className='option' to='/shop'>
+                Shop
+            </Link>
+            <Link className='option' to='/shop'>
+                Shop
+            </Link>
+            <Link className='option' to='/shop'>
+                Shop
+            </Link>
+            <Link className='option' to='/shop'>
+                Shop
+            </Link>
+        </div>
+    </div>
+)
+
+export default Header;
