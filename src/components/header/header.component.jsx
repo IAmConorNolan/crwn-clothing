32d92bc8491464ac730d37ef1093933f7580e88f
IAmConorNolan <nolanc14@tcd.ie>
2019-07-19 02:11:52 +0100

    Added cart reducer and cart funtionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/img/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -27,12 +29,17 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to='/sign-in'>Sign in</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header); //Now currentUser is mapped to the state's current user.
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,12 @@
+import { UserActionTypes } from './user.types';
+
 const INITIAL_STATE = {
     currentUser: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => { // INITIAL_STATE is default
     switch (action.type) {
-        case 'SET_CURRENT_USER': //WHENEVER THE TYPE OF ACTION IS SET CURRENT USER
+        case UserActionTypes.SET_CURRENT_USER: //WHENEVER THE TYPE OF ACTION IS SET CURRENT USER
             return {
                 ...state,
                 currentUser: action.payload, //RETURNS THE CURRENT STATE, + PAYLOAD AS A NEW OBJECT
