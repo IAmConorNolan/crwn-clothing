d2f95d65380a3151b6c521b55d38278db2ad8b6e
IAmConorNolan <nolanc14@tcd.ie>
2019-07-19 03:11:34 +0100

    Added ability to add items to cart, and added quantity property to items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './collection-preview.styles.scss';
 
-import {CollectionItem} from '../collection-item/collection-item.component';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title}</h1>
-        <div className='preview'> {/* MUST REMEMBER THAT EVERYTHING IN HERE, INLCUIDNG THESE FUNCTIONS GET CALLED WHENEVER COLLECTIONSPREVIEW IS RERENDERED */}
-            {
-                items
-                .filter((i, idx) => idx < 4)
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
-                ))
+        <div className='preview'> 
+            {items
+            .filter((item, idx) => idx < 4)
+            .map(item => (
+               <CollectionItem key={item.id} item={item} />
+            ))
             }
         </div>
     </div>
 );
 
 export default CollectionPreview;
+
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,13 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(el => el.id === cartItemToAdd.id);
+
+    if (existingCartItem) {
+        return cartItems.map(el => 
+            el.id === cartItemToAdd.id
+            ? { ...el, quantity: el.quantity + 1 }
+            : el
+        )
+    }
+
+    return [...cartItems, {...cartItemToAdd, quantity: 1}];
+};
